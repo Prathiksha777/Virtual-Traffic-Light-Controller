d10c9a7f26f94d0b9f9e8016f7215b0853288212
Shiva-Raj-Kumar <shivarajkumar@iast-software.com>
2025-06-06 23:28:28 +0530

    Update main.cpp
    Created Traffic controller Thread

--- CODE/src/main.cpp
+++ CODE/src/main.cpp
@@ -6,8 +6,14 @@ using namespace std;
 using namespace chrono;
 
 int main() {
-   
-   
+   TrafficController controller;
+
+    thread trafficThread([&controller]() {
+        controller.runController();
+    });
+
+    // Add TEST CASES here
+    trafficThread.join();
     return 0;
 }
 
